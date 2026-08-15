0b56219d4e2ee3c0982eb9b94be6349d9b469506
asepaja7408-netizen <asepaja7408@gmail.com>
2026-08-16 04:10:27 +0700

    Modify configuration settings in Config.lua
    Updated allowed place IDs and developer information. Changed URLs for WhatsApp and Buy Key.

--- Config.lua
+++ Config.lua
@@ -4,18 +4,18 @@ return {
     CLONE_BATCH_SIZE = 5,
     CLONE_DELAY = 6,
     REMOTE_PATH = "Remotes.Command.CommandEvent",
-    ALLOWED_PLACE_IDS = {133943904733338, 7041939546},
-    DEVELOPER_USERNAME = "AlfreadR0rw",
+    ALLOWED_PLACE_IDS = {2446060691},
+    DEVELOPER_USERNAME = "asep",
     DEVELOPER_USER_ID = 2446060691,
     KEY_TIMER_INTERVAL = 1, -- detik
 
-    BUY_KEY_URL = "https://wa.me/60136951175?text=beli",
+    BUY_KEY_URL = "https://wa.me/6285788699048?text=beli",
 
     LogoURL = "https://files.catbox.moe/io8o2d.png",
     LogoLocalPath = "PhoneIDViewer_Logo.png",
 
     DiscordURL = "https://discord.gg/yourdiscord",
-    WhatsAppURL = "https://wa.me/6281234567890",
+    WhatsAppURL = "https://wa.me/6285788699048",
     TelegramURL = "https://t.me/yourusername",
 
     DiscordIconURL = "https://files.catbox.moe/lgxgw4.jpg",
